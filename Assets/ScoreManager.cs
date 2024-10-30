5de0b801df12bf598e5cb00b4a4e8684fc5cfd89
Cactus-EXE <77512312+Cactus-EXE@users.noreply.github.com>
2024-10-29 20:29:41 -0500

    Score Persistance and ui fixes
    Fixed some of the main menu ui issues and made the score persist between screens

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using TMPro;
+
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
-
     private int score = 0;
 
     void Start()
     {
+        LoadScore();
         UpdateScoreText();
     }
 
@@ -23,9 +24,30 @@ public class ScoreManager : MonoBehaviour
         }
     }
 
+    void LoadScore()
+    {
+        score = PlayerPrefs.GetInt("CurrentScore", 0);  // Loads score, default to 0 if not set
+    }
+
+    public void SaveScore()
+    {
+        PlayerPrefs.SetInt("CurrentScore", score);  // Saves score to PlayerPrefs
+        PlayerPrefs.Save();  // Make sure to save PlayerPrefs
+    }
+
     public void OnBananaClicked()
     {
         score += 1;
         UpdateScoreText();
+    }
+
+    void OnDisable()
+    {
+        SaveScore();  // Save the score when the game object is disabled or the scene is unloaded
+    }
+
+    void OnEnable()
+    {
+        LoadScore();  // Load the score when the game object is enabled
     }
 }
