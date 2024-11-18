01068f165f9c6bc9878cb20ee318d99827c5cc41
ndow1021 <nathandow1021@gmail.com>
2024-11-18 11:46:54 -0600

    WEEZERRRRR
    Added Weezer scenes and beginning of scripting

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -16,7 +16,14 @@ public class MainMenu : MonoBehaviour
            
         
     }
+  public void PlayGameWEEZER()
+  {
+        // Load the next scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameplaySceneWEEZER");
 
+
+
+  }
 
     public void QuitGame()
     {
--- Assets/shopButtonWEEZER.cs
+++ Assets/shopButtonWEEZER.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class shopButton : MonoBehaviour
+public class shopButtonWEEZER : MonoBehaviour
 {
-    [SerializeField] private string shopScreen = "Shop";
     [SerializeField] private string shopScreenWEEZER = "ShopWEEZER";
-    public void ShopButton()
-    {
-        SceneManager.LoadScene(shopScreen);
-    }
+
     public void ShopButtonWEEZER()
     {
         SceneManager.LoadScene(shopScreenWEEZER);
